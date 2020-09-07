82dd62b15de34295b981c78f58ec219b56f5a4a6
Kevin <kkubwawe@gmail.com>
2020-09-07 16:21:15 +0200

    palidrome.exe

--- cpp_td1/palindrome.cpp
+++ cpp_td1/palindrome.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
     string word;
+    cout<<"Enter a word: ";
     cin>>word;
-    cout<<word<<endl;
+    const char *cstr = word.c_str();
+    int n= word.length();
+    bool isPalindrome=true;
+    for(int i=0;i < n  ;i++)
+    {
+        if(cstr[i] != cstr[n-i-1])
+        {
+            isPalindrome = false;
+            break;
+        }
+    }
+    if(isPalindrome)
+        cout<<word<<" "<<"is a palindrome."<<endl;
+    else{
+        cout<<word<<" "<<"is not a palindrome."<<endl;
+    }
     return 0;
 }
